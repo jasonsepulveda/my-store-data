eb4ea12b0318cc5b98eb0e382615092b5208997d
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-09-25 21:14:24 -0500

    class 7 - pool conection

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const OrderService = require('./../services/order.service');
 
 const router = express.Router();
+const service = new OrderService();
 
-router.get('/', (req, res) => {
-  res.json([]);
+router.get('/', async (req, res, next) => {
+  try {
+    const orders = await service.find();
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- services/category.service.js
+++ services/category.service.js
@@ -1,15 +1,20 @@
 const boom = require('@hapi/boom');
+const pool = require('./../libs/postgres.pool')
 
 class CategoryService {
 
   constructor(){
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
   async create(data) {
     return data;
   }
 
   async find() {
-    return [];
+    const query = 'SELECT * FROM tasks';
+    const response = await this.pool.query(query)
+    return response.rows;
   }
 
   async findOne(id) {
