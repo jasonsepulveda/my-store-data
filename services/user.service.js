30ede67e6d1f82c7b95e4d2477b9848c9f98dc93
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-09-30 14:53:04 -0500

    class 11 - sequelize, ORM and CRUD with sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -1,35 +1,38 @@
 const boom = require('@hapi/boom');
-const pool = require('./../libs/postgres.pool')
+
+const { models } = require('./../libs/sequelize')
 
 class UserService {
 
-  constructor() {
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
-  }
+  constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const response = await this.pool.query(query)
-    return response.rows;
+    const response = await models.User.findAll();
+    return response;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user){
+      throw boom.notFound('User not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const response = await user.update(changes);
+    return response;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
