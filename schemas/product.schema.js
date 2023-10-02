a896523bc2dbbf9a67de3802cf90c907e9b081b5
Jason Sepulveda <jasonsepulvedas@gmail.com>
2023-10-02 16:28:34 -0500

    class 24 - price filer with intervals

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -10,6 +10,10 @@ const categoryId = Joi.number().integer();
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
 
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
+
+
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -33,7 +37,13 @@ const getProductSchema = Joi.object({
 
 const queryProductSchema = Joi.object({
   limit,
-  offset
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required()
+  })
 });
 
 
